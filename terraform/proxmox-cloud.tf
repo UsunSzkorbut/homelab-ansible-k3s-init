442e79a1e07ca827f4ed64e98524479f7146a64c
UsunSzkorbut <wojtek.hajduk.2000@gmail.com>
2024-08-19 21:36:59 +0200

    Update proxmox terraform, change ram and cpu with uncomment

--- terraform/proxmox-cloud.tf
+++ terraform/proxmox-cloud.tf
@@ -12,7 +12,7 @@ resource "proxmox_vm_qemu" "cloud-k3s-master" {
     sockets = 1
     vcpus = 0
     cpu = "host"
-    memory = 8096
+    memory = 8192
     name = "k3s-master-0${count.index + 1}"
 
     scsihw   = "virtio-scsi-single" 
@@ -43,47 +43,47 @@ resource "proxmox_vm_qemu" "cloud-k3s-master" {
     ipconfig0 = "ip=10.0.0.10/24,gw=10.0.0.1"
 }
 
-# resource "proxmox_vm_qemu" "cloud-k3s-worker" {
-#     target_node = "homelab"
-#     desc = "Cloud Ubuntu 22.04"
-#     count = 2
-#     onboot = true
+resource "proxmox_vm_qemu" "cloud-k3s-worker" {
+    target_node = "homelab"
+    desc = "Cloud Ubuntu 22.04"
+    count = 2
+    onboot = true
 
-#     clone = "ubuntu-cloud"
-#     agent = 0
+    clone = "ubuntu-cloud"
+    agent = 0
 
-#     os_type = "cloud-init"
-#     cores = 2
-#     sockets = 1
-#     vcpus = 0
-#     cpu = "host"
-#     memory = 5096
-#     name = "k3s-worker-0${count.index + 1}"
+    os_type = "cloud-init"
+    cores = 1
+    sockets = 1
+    vcpus = 0
+    cpu = "host"
+    memory = 4096
+    name = "k3s-worker-0${count.index + 1}"
 
-#     scsihw   = "virtio-scsi-single" 
-#     bootdisk = "scsi0"
-#     disks {
-#         scsi {
-#             scsi0 {
-#                 disk {
-#                   storage = "local-lvm"
-#                   emulatessd = true
-#                   size = 150
-#                 }
-#             }
-#         }
-#         ide {
-#             ide3 {
-#                 cloudinit {
-#                   storage = "local-lvm"
-#                 }
-#             }
-#         }
-#     }
+    scsihw   = "virtio-scsi-single"
+    bootdisk = "scsi0"
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  emulatessd = true
+                  size = 150
+                }
+            }
+        }
+        ide {
+            ide3 {
+                cloudinit {
+                  storage = "local-lvm"
+                }
+            }
+        }
+    }
 
-#     network {
-#         model = "virtio"
-#         bridge = "vmbr0"
-#     }
-#     ipconfig0 = "ip=10.0.0.2${count.index + 1}/24,gw=10.0.0.1"
-# }
+    network {
+        model = "virtio"
+        bridge = "vmbr0"
+    }
+    ipconfig0 = "ip=10.0.0.2${count.index + 1}/24,gw=10.0.0.1"
+}
