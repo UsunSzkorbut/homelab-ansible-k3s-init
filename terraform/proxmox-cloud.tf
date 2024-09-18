827244a12ab9d915846355877b1ec3d844399087
UsunSzkorbut <wojtek.hajduk.2000@gmail.com>
2024-09-18 16:17:23 +0200

    Change configs to fit 2 node cluster

--- terraform/proxmox-cloud.tf
+++ terraform/proxmox-cloud.tf
@@ -8,11 +8,11 @@ resource "proxmox_vm_qemu" "cloud-k3s-master" {
     agent = 0
 
     os_type = "cloud-init"
-    cores = 1
+    cores = 2
     sockets = 1
     vcpus = 0
     cpu = "host"
-    memory = 8192
+    memory = 15360
     name = "k3s-master-0${count.index + 1}"
 
     scsihw   = "virtio-scsi-single" 
@@ -23,7 +23,7 @@ resource "proxmox_vm_qemu" "cloud-k3s-master" {
                 disk {
                   storage = "local-lvm"
                   emulatessd = true
-                  size = 50
+                  size = 200
                 }
             }
         }
@@ -41,49 +41,4 @@ resource "proxmox_vm_qemu" "cloud-k3s-master" {
         bridge = "vmbr0"
     }
     ipconfig0 = "ip=10.0.0.1${count.index + 1}/24,gw=10.0.0.1"
-}
-
-resource "proxmox_vm_qemu" "cloud-k3s-worker" {
-    target_node = "homelab"
-    desc = "Cloud Ubuntu 22.04"
-    count = 2
-    onboot = true
-
-    clone = "ubuntu-cloud"
-    agent = 0
-
-    os_type = "cloud-init"
-    cores = 1
-    sockets = 1
-    vcpus = 0
-    cpu = "host"
-    memory = 8192
-    name = "k3s-worker-0${count.index + 1}"
-
-    scsihw   = "virtio-scsi-single"
-    bootdisk = "scsi0"
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                  storage = "local-lvm"
-                  emulatessd = true
-                  size = 150
-                }
-            }
-        }
-        ide {
-            ide3 {
-                cloudinit {
-                  storage = "local-lvm"
-                }
-            }
-        }
-    }
-
-    network {
-        model = "virtio"
-        bridge = "vmbr0"
-    }
-    ipconfig0 = "ip=10.0.0.2${count.index + 1}/24,gw=10.0.0.1"
 }
