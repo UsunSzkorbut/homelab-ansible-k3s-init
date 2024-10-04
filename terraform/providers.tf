9425222ce96020c125e0522ed60621155fa0729a
UsunSzkorbut <96394695+UsunSzkorbut@users.noreply.github.com>
2024-10-04 20:46:26 +0200

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "proxmox" {
     pm_tls_insecure = true
-    pm_api_url = "https://10.0.0.111:8006/api2/json"
-    pm_api_token_secret = "2950cc78-bd7f-45de-80c1-9d5833c5e440"
+    pm_api_url = "https://X.X.X.X:8006/api2/json"
+    pm_api_token_secret = "<YOUR_API_TOKEN>"
     pm_api_token_id = "root@pam!terraform"
 }
